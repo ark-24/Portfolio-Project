85ddea487b3ed2bf0e77afc7508a2eee3fee3662
ark24 <abdul.khan@designitcorp.ca>
2023-09-09 02:54:50 -0700

    Added Home Page Content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import ParticlesBackground from './Particles/ParticlesBackground';
 import './Home.scss'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Stack from "@mui/material/Stack";
 
 const Home = () => {
   return (
     <div  >
     <ParticlesBackground/>
+    <div className='info'>
     <span className='intro'> Hi, I'm Abdul</span>
+    <span className='aboutMe'>I'm a software engineer driven by a passion for crafting inventive and effective solutions to intricate challenges. My expertise spans across diverse sectors, including medical software and web development, equipping me with the skills to leverage cutting-edge technologies for organizational growth and achievement. 
+      Feel free to explore my resume and connect with me on LinkedIn for more insights into my profile!</span>
+    </div>
+    <div className="icons">
+    <LinkedInIcon onClick={event => window.open('https://www.google.com', '_blank')} color="primary" fontSize="large" sx={{display: 'flex',
+    justifyContent: 'center'}} />
 
+    </div>
     </div>
   )
 }
